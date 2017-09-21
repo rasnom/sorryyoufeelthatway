6d3294f8dd64b6269ff7c0158eb3a26d54835008
Ransom <ransom.byers@gmail.com>
2017-09-21 00:03:17 -0700

    Add CardTemplate migration and seed data

---
+++ app/models/card_template.rb
@@ -0,0 +1,2 @@
+class CardTemplate < ApplicationRecord
+end
--- spec/controllers/card_templates_controller_spec.rb
+++ spec/controllers/card_templates_controller_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe CardTemplatesController, type: :controller do
       get :index
       expect(response).to render_template('index')
     end
+
+    it
   end
 
 end
