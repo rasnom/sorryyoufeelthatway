3aec6d6b62841fb2e98f523a37f5e07699684f00
Ransom <ransom.byers@gmail.com>
2017-09-22 15:58:40 -0700

    Add test for creating a valid new Card

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:template) { CardTemplate.create(greeting: 'oh noes', image_file: 'oh.noes') }
+  let(:valid_inputs) {{
+    card_template_id: template.id,
+    custom_message: 'Not having the sorry',
+    signature: 'Kindly, James',
+    recipient_name: 'Bad Friend',
+    street_address: '123 Main Street',
+    city: 'Oakland',
+    state: 'CA',
+    zip_code: '04294'
+  }}
+
+  describe 'Creating a new card' do
+    it 'can create a new card with all the required fields' do
+      card = Card.new(valid_inputs)
+      expect(card.save).to be true
+    end
+  end
+
 end
