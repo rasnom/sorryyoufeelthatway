6b5e8be21f10e22515469f94fce0b410bd45959a
Ransom <ransom.byers@gmail.com>
2017-09-23 00:21:59 -0700

    Add html5 validations to new card form

--- spec/features/ordering_spec.rb
+++ spec/features/ordering_spec.rb
@@ -11,10 +11,8 @@ describe 'Customizing and ordering a card' do
   end
 
   describe 'Customizing and ordering a card' do
-    it 'Can create a card with all of the relevant fields' do
+    before(:each) do
       visit "/card_templates/#{template.id}"
-      card_count = Card.all.count
-
       fill_in_card_form(
         custom_message: 'Not having the sorry',
         signature: 'Kindly, James',
@@ -24,11 +22,23 @@ describe 'Customizing and ordering a card' do
         state: 'CA',
         zip_code: '94607'
       )
+    end
+
+    it 'Can create a card with all of the relevant fields' do
+      card_count = Card.all.count
       click_on 'Send Card'
 
       expect(page).to have_content 'It is done'
       expect(Card.all.count).to eq (card_count + 1)
     end
+
+    it 'Does not accept form submission without any of the required fields' do
+      # It looks like there is no great way to directly check html5 form
+      # behavior with rspec and capybara. There are tests for preventing a
+      # new card without required fields from being saved, so I think we can
+      # set this aside to possibly revisit later.
+    end
+
   end
 
   def fill_in_card_form(custom_message:, signature:, recipient_name:, street_address:, city:, state:, zip_code:)
