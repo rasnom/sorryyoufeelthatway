12253a2dce7807d6e8d16eec703125da90bbd7d1
Ransom <ransom.byers@gmail.com>
2017-09-23 18:38:58 -0700

    Add Charges controller

---
+++ spec/controllers/charges_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe ChargesController, type: :controller do
+
+  def new
+  end
+
+  def create
+    #amount is in cents
+    @amount = 499
+
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd'
+    )
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
+  end
+end
