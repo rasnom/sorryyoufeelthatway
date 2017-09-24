af2f56019724f2a866b9540be7eb75c5c7ce2303
Ransom <ransom.byers@gmail.com>
2017-09-23 19:17:59 -0700

    Move controller methods to the right file

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,27 @@
 class ChargesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #amount is in cents
+    @amount = 499
+
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd'
+    )
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
+  end
 end
