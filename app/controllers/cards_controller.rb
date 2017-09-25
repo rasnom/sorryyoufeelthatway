74a2f6ec4cf95784eb0a23dab5d4306f68334775
Ransom <ransom.byers@gmail.com>
2017-09-24 23:43:35 -0700

    Integrate Strip charge creation into cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,10 +8,29 @@ class CardsController < ApplicationController
     p new_card.save
     p '############################################'
 
-
-    if new_card.save
-      redirect_to congrats_path
+    unless new_card.save
+      flash[:error] = "Unable to create this card for some reason."
+      redirect_to "/card_templates/#{new_card.card_template_id}"
     end
+
+     #amount is in cents
+    @amount = 499
+
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Sorry you feel that way . biz Customer',
+      currency: 'usd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to "/card_templates/#{new_card.card_template_id}"
   end
 
   private
