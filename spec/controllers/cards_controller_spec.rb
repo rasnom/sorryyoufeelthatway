288dc4e8f807a7ee090d62ae2a87e2b4d71d9fdd
Ransom <ransom.byers@gmail.com>
2017-09-28 22:36:27 -0700

    Add pending tests for cards controller

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe CardsController, type: :controller do
 
+  descripe 'POST create' do
+    describe 'If all the necessary params are included and valid' do
+      xit 'Creates a new card' do
+
+      end
+
+      xit 'Reloads the page with the flag to enable checkout' do
+
+      end
+
+    describe 'If there are problems with the parmams' do
+      xit 'Rerenders the page with an error' do
+
+      end
+
+      xit 'Autofills the fields with the existing input' do
+
+      end
+    end
+  end
 end
