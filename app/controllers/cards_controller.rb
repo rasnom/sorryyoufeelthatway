163c46c1ef9278c3651a2a04db38c5a6f1b20896
Ransom <ransom.byers@gmail.com>
2017-10-01 23:30:49 -0700

    Start to separate the card and charge creation. Add controller test for cards#create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,29 +8,29 @@ class CardsController < ApplicationController
       redirect_to "/card_templates/#{new_card.card_template_id}"
     end
 
-     #amount is in cents
-    @amount = 499
+  #    #amount is in cents
+  #   @amount = 499
 
-    customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken]
-    )
+  #   customer = Stripe::Customer.create(
+  #     email: params[:stripeEmail],
+  #     source: params[:stripeToken]
+  #   )
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: @amount,
-      description: 'Sorry you feel that way . biz Customer',
-      currency: 'usd',
-      metadata: {
-        card_id: new_card.id,
-        card_template_id: new_card.card_template_id,
-        message: new_card.custom_message
-      }
-    )
+  #   charge = Stripe::Charge.create(
+  #     customer: customer.id,
+  #     amount: @amount,
+  #     description: 'Sorry you feel that way . biz Customer',
+  #     currency: 'usd',
+  #     metadata: {
+  #       card_id: new_card.id,
+  #       card_template_id: new_card.card_template_id,
+  #       message: new_card.custom_message
+  #     }
+  #   )
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to "/card_templates/#{new_card.card_template_id}"
+  # rescue Stripe::CardError => e
+  #   flash[:error] = e.message
+  #   redirect_to "/card_templates/#{new_card.card_template_id}"
   end
 
   private
--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CardsController, type: :controller do
+  let(:template) { CardTemplate.create(greeting: "Whoa Nellie", image_file: "wn.png") }
+  let(:card_params) {
+    {
+      card_template_id: template.id,
+      custom_message: 'errrr.....',
+      signature: '-neemur neemur',
+      recipient_name: 'the bees',
+      street_address: 'over yonder',
+      city: 'Oakland',
+      state: 'CA',
+      zip_code: '04294'
+    }
+  }
 
   describe 'POST create' do
     describe 'If all the necessary params are included and valid' do
-      xit 'Creates a new card' do
-
+      it 'Creates a new card' do
+        post :create, params: { card_template_id: template.id, card: card_params }
+        expect(Card.last[:custom_message]).to eq card_params[:custom_message]
       end
 
       xit 'Reloads the page with the flag to enable checkout' do
