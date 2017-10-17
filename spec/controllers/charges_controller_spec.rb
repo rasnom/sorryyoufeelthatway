7e432d38ccbe2e21825d49b10c97412f4f40a379
Ransom <ransom.byers@gmail.com>
2017-10-17 00:08:46 -0700

    Add create method to charges controller

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ChargesController, type: :controller do
 
+  describe 'POST create' do
+    it 'sets @amount to 499 cents' do
+      post :create
+      expect(assigns(:amount)).to eq 499
+    end
+  end
+
 end
