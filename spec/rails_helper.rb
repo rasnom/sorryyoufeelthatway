805ab6133d1a291d1373f9719f8fa3125af18940
Ransom <ransom.byers@gmail.com>
2017-10-19 22:53:06 -0700

    Add chrome addon to Travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,9 +14,9 @@ Capybara.register_driver :headless_chrome do |app|
     chromeOptions: { args: %w(headless disable-gpu) }
   )
 
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+Capybara::Selenium::Driver.new app,
+  browser: :chrome,
+  desired_capabilities: capabilities
 end
 
 Capybara.javascript_driver = :headless_chrome
