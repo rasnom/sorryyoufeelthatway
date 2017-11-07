49d4e90fdd323c7c98bb0e2f71a0734b5064eb90
Ransom <ransom.byers@gmail.com>
2017-11-06 21:53:36 -0800

    Add admin_controller#index method and view

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe AdminController, type: :controller do
-
+RSpec.describe AdminController, type: :controller, admin: true do
+  describe 'GET index' do
+    it 'renders the admin index template' do
+      get :index
+      expect(response).to render_template 'index'
+    end
+  end
 end
